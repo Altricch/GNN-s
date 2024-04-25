62828f6a54f8c1c7cca9b19234aa5ae48bbb5c22
Frakk0 <huberf@usi.ch>
2024-04-25 16:19:14 +0200

    Functioning GGNN

--- GGNN.py
+++ GGNN.py
@@ -1,4 +1,5 @@
 # Gated Graph nn
+# from paper: GATED GRAPH SEQUENCE NEURAL NETWORKS
 import os
 import os.path as osp
 import torch
@@ -9,16 +10,19 @@ import torch_geometric
 from torch_geometric.datasets import Planetoid, TUDataset
 from torch_geometric.data import DataLoader
 from torch_geometric.nn.inits import uniform
+from torch_geometric.nn.conv import MessagePassing
 from torch.nn import Parameter as Param
 from torch import Tensor
-
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 torch.manual_seed(42)
+
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = "cpu"
-from torch_geometric.nn.conv import MessagePassing
 
 dataset = 'PubMed'
 transform = T.Compose([T.TargetIndegree(),
@@ -49,6 +53,8 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.mlp(x)
 
+
+#BASE Class for information propagation and update amongst nodes
 class GatedGraphConv(MessagePassing):
 
     def __init__(self, out_channels, num_layers, aggr = 'add',
@@ -70,6 +76,7 @@ class GatedGraphConv(MessagePassing):
     def forward(self, data):
         """"""
         x = data.x
+
         edge_index = data.edge_index
         edge_weight = data.edge_attr
         if x.size(-1) > self.out_channels:
@@ -100,34 +107,101 @@ class GatedGraphConv(MessagePassing):
                                               self.num_layers)
 
 class GGNN(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, out_channels, num_conv=3, hidden_dim = 32, aggr = 'add'):
         super(GGNN, self).__init__()
 
-        self.conv = GatedGraphConv(1433, 3)
-        self.mlp = MLP(1433, [32,32,32], dataset.num_classes)
+        self.conv = GatedGraphConv(out_channels=out_channels,
+                                   num_layers=num_conv)
+
+        self.mlp = MLP(input_dim= in_channels,
+                       hid_dims= [32,32,32],
+                       out_dim= dataset.num_classes)
 
     def forward(self):
         x = self.conv(data)
         x = self.mlp(x)
         return F.log_softmax(x, dim=-1)
 
+# def train():
+#     model.train()
+#     optimizer.zero_grad()
+#     loss_fn(model()[data.train_mask], data.y[data.train_mask]).backward()
+#     optimizer.step()
+#
+# def test(model):
+#     model.eval()
+#     logits, accs = model(), []
+#     for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+#         pred = logits[mask].max(1)[1]
+#         acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+#         accs.append(acc)
+#     return accs
 
-def train():
-    model.train()
-    optimizer.zero_grad()
-    loss_fn(model()[data.train_mask], data.y[data.train_mask]).backward()
-    optimizer.step()
+def train(dataset, epochs=100):
+    start_time = time.time()
+    test_loader = loader = DataLoader(dataset, batch_size=1, shuffle=True)
+    model = GGNN(in_channels=500, out_channels=500).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_fn = nn.CrossEntropyLoss()
 
+    tot_loss = 0
 
-def test(): 
-    model.eval()
-    logits, accs = model(), []
-    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
-        pred = logits[mask].max(1)[1]
-        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
-        accs.append(acc)
-    return accs
+    best_acc = [0,0,0]
 
+    print("#" * 20 + f" Running Gated GNN, with {str(epochs)} epochs, {None} convs" + "#" * 20)
+
+    for epoch in range(1, epochs+1):
+        print(f'Processing Epoch {epoch}', end="\r")
+        epoch_start_time = time.time()
+        ### Train
+        model.train()
+
+        for batch in loader:
+
+            optimizer.zero_grad()
+
+            pred = model()[batch.train_mask]
+            label = batch.y[batch.train_mask]
+
+            loss = loss_fn(pred, label)
+            loss_fn(pred, label).backward()
+
+            optimizer.step()
+            tot_loss += loss.item() # TODO verify need for batch.num_graphs
+
+        tot_loss /= len(loader.dataset)
+            ###
+
+        ### Test
+        #MODIFICATION, evaluation now every 10 steps rather than natively 1to1
+        if epoch % 10 == 0:
+            model.eval()
+            logits, accs = model(), []
+            #for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+
+            for data in test_loader:
+                masks = [data.train_mask, data.val_mask, data.test_mask]
+                for mask in masks:
+                    pred = logits[mask].max(1)[1]
+                    acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+                    accs.append(acc)
+
+            train_acc = accs[0]
+            val_acc = accs[1]
+            test_acc = accs[2]
+
+            best_acc[0] = max(best_acc[0],train_acc)
+            best_acc[1] = max(best_acc[1],val_acc)
+            best_acc[2] = max(best_acc[2],test_acc)
+
+            print('Epoch: {:03d}, Train Acc: {:.0%}, '
+                  'Val Acc: {:.0%}, Test Acc: {:.0%}, time for 10 epochs {:.2f}'.format(epoch, train_acc,
+                                                             val_acc, test_acc, time.time() - epoch_start_time))
+
+    print("Training Completed in {:.2f} seconds".format(time.time() - start_time))
+    print("Best Accuracies Train Acc: {:.0%}, Val Acc: {:.0%}, Test Acc: {:.0%}".format(best_acc[0], best_acc[1],  best_acc[2]))
+
+    return model
 
 def visualization_nodembs(dataset, model):
     color_list = ["red", "orange", "green", "blue", "purple", "brown", "black"]
@@ -146,14 +220,6 @@ def visualization_nodembs(dataset, model):
     embs = torch.cat(embs, dim=0)
 
     xs, ys = zip(*TSNE(random_state=42).fit_transform(embs.detach().numpy()))
-    # print("xs shape is", xs)
-    # print("ys shape is", len(xs))
-
-    # max_distance = calculate_diameter(xs, ys)
-    # eccentricity = calculate_eccentricity(xs, ys)
-
-    # print(f"Max distance: {max_distance}")
-    # print(f"Eccentricity: {eccentricity}")
 
     plt.scatter(xs, ys, color=colors)
     plt.title( "swag")
@@ -161,26 +227,24 @@ def visualization_nodembs(dataset, model):
     plt.show()
 
 
+### Flags Areas ###
+import argparse
+parser = argparse.ArgumentParser(description='Process some inputs.')
+parser.add_argument('--epoch', type=int, help='Epoch Amount', default=100)
+parser.add_argument('--conv', type=int, help='Conv Amount', default=3)
 
 
 if __name__ == '__main__':
-    model = GGNN().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    loss_fn = nn.CrossEntropyLoss()
 
     test_dataset = dataset[:len(dataset) // 10]
     train_dataset = dataset[len(dataset) // 10:]
     test_loader = DataLoader(test_dataset)
     train_loader = DataLoader(train_dataset)
 
-    for epoch in range(1, 2):
-        train()
-        accs = test()
-        train_acc = accs[0]
-        val_acc = accs[1]
-        test_acc = accs[2]
-        print('Epoch: {:03d}, Train Acc: {:.5f}, '
-              'Val Acc: {:.5f}, Test Acc: {:.5f}'.format(epoch, train_acc,
-                                                         val_acc, test_acc))
+    args = parser.parse_args()
 
-    visualization_nodembs(dataset, model)
+    epochs = args.epoch
+    model = train(dataset, epochs=epochs)
+
+    model.__repr__()
+    #visualization_nodembs(dataset, model)
